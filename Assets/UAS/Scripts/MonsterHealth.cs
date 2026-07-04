2c991301bd6fca1fd58b133cf1402a4908aea3b9
devaagriani <deva.agriani@cakrawala.ac.id>
2026-07-04 15:24:28 +0700

    animasi dan character interactable

--- Assets/UAS/Scripts/MonsterHealth.cs
+++ Assets/UAS/Scripts/MonsterHealth.cs
@@ -5,6 +5,27 @@ public class MonsterHealth : MonoBehaviour {
     public int maxHitCount = 10; // Dibutuhkan 10 hit untuk hancur
     private int currentHitCount = 0;
 
+    [Header("Floating Animation Settings")]
+    [Tooltip("Seberapa tinggi monster mengambang naik-turun")]
+    public float floatAmplitude = 0.5f; 
+    [Tooltip("Seberapa cepat gerakan naik-turunnya")]
+    public float floatSpeed = 2f; 
+
+    private Vector3 startPosition;
+
+    void Start() {
+        // Simpan posisi awal monster saat game dimulai
+        startPosition = transform.position;
+    }
+
+    void Update() {
+        // Menghitung pergeseran posisi menggunakan rumus Sinus
+        float newY = startPosition.y + Mathf.Sin(Time.time * floatSpeed) * floatAmplitude;
+        
+        // Terapkan posisi baru ke monster
+        transform.position = new Vector3(startPosition.x, newY, startPosition.z);
+    }
+
     // Fungsi ini akan dipanggil oleh sistem senjata ketika mengenai monster
     public void TakeDamage() {
         currentHitCount++;
